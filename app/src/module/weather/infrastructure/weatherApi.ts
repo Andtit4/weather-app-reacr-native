b0b036e3500254c72fd8d85461492c3e4eac04e4
Andtit4 <lirsitogo2021@gmail.com>
2025-07-24 06:29:23 +0000

    update

--- app/src/module/weather/infrastructure/weatherApi.ts
+++ app/src/module/weather/infrastructure/weatherApi.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { Weather } from "../domain/entities/Weather";
 import { IWeatherRepository } from "../domain/repositories/IWeatherRepository";
+import * as dotenv from "dotenv"
+dotenv.config()
 
 export class WeatherApi implements IWeatherRepository {
     async getWeather(city: string): Promise<Weather> {
         // throw new Error("Method not implemented.");
-        const API_KEY = '32a3c53ee027e88f725ebe8cfdb5f468'
+        const API_KEY = process.env.API_KEY
         const res = await axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
         );
